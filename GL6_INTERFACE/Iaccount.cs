c519aa1dfcac618a2c78ee6144a9ebb7a2c0b8a5
Kuralenok <104193785+Kuralenok@users.noreply.github.com>
2022-04-27 11:33:22 +0300

    Доделал задание

---
+++ GL6_INTERFACE/Iaccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Интерфейсы
+{
+    interface Iaccount
+    {
+        void put(double summa);
+
+        void get(double summa);
+
+        void percent();
+    }
+}
--- GL6_INTERFACE/Program.cs
+++ GL6_INTERFACE/Program.cs
@@ -1,123 +1,6 @@
 ﻿using System;
 namespace Интерфейсы
 {
-    interface Iaccount
-    {
-       
-        void put(double summa);
-     
-        void get(double summa);
-
-
-
- void percent();
-    }
-    interface Ibonus
-    {
-       
-        double bonus();
-    }
-    class Bank_Client : Iaccount, Ibonus
-    {
-        string name; 
-        int passport; 
-        double summa_bank;
-        DateTime date; 
-
-        public double Summa_bank
-        { 
-            get { return summa_bank; }
-        }
-
-        public Bank_Client(string name,
-        int passport, double summa_bank,
-        int year, int month, int day)
-        {
-            this.name = name;
-            this.passport = passport;
-            this.summa_bank = summa_bank;
-            date = new DateTime(year, month, day);
-        }
-
-        public void Person_Display()
-        {
-            Console.WriteLine(" " + name + " " + passport +
-            " " + summa_bank + " " + date);
-        }
-       
-       
-        public void put(double summa)
-        {
-            summa_bank += summa;
-        }
-        
-        public void get(double summa)
-        {
-            if (summa <= summa_bank)
-                summa_bank -= summa;
-        }
-
-
-
- public void percent()
-        {
-            DateTime today = DateTime.Today;
-            if ((today - date).Days == 365)
-                summa_bank *= 1.1;
-        }
-
-        public double bonus()
-        {
-            double add_bonus = 0.0;
-            DateTime today = DateTime.Today;
-            DateTime endOfYear =
-            new DateTime(today.Year, 12, 31);
-            if (today == endOfYear)
-            {
-                int summa_days =
-                (endOfYear - date).Days;
-                if (summa_bank > 1000000 &&
-                summa_days > 180)
-                    add_bonus = summa_bank * 0.005;
-                Console.WriteLine
-                (" Бонус начислен" + add_bonus);
-            }
-            return (add_bonus);
-        }
-    } 
-    class Shop_Client : Ibonus
-    {
-        double summa_shop; 
-        double summa_buy; 
-        public double Summa_buy
-        { 
-            get { return summa_buy; }
-            set { summa_buy = value; }
-        }
-        public Shop_Client()
-        { 
-            summa_shop = 0; summa_buy = 0;
-        }
-
-
- 
- public double bonus()
-        {
-            double add_bonus = 0.0;
-            if (summa_shop > 40000)
-                add_bonus = summa_buy * 0.05;
-            else
-            if (summa_shop > 30000)
-                add_bonus = summa_buy * 0.02;
-            return (add_bonus);
-        }
-        public void New_Buy()
-        { 
-            Console.WriteLine("Введите сумму покупки");
-            summa_buy = int.Parse(Console.ReadLine());
-            summa_shop += summa_buy;
-        }
-    }
     class Program
     {
         public static void Main()
@@ -127,24 +10,24 @@ namespace Интерфейсы
             client.put(50000); client.get(20000);
             client.Person_Display();
             client.percent();
-            
+
+
             client.put(client.bonus());
             Console.WriteLine
             (" сумма вклада: " + client.Summa_bank);
-            
+
             Shop_Client client1 = new Shop_Client();
-            client1.New_Buy(); 
+            client1.New_Buy();
             client1.Summa_buy -= client1.bonus();
             Console.WriteLine
             ("к оплате: " + client1.Summa_buy);
-            client1.New_Buy(); 
+            client1.New_Buy();
             client1.Summa_buy -= client1.bonus();
             Console.WriteLine
-       
 
- ("к оплате: " + client1.Summa_buy);
+
+           ("к оплате: " + client1.Summa_buy);
         }
     }
 }
 
-        
